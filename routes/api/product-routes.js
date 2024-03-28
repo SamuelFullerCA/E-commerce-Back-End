d8a0c29aadf3c67240168e7d7e2a67b3e71843d8
SamuelFullerCA <samuelfullerca@gmail.com>
2024-03-28 11:40:32 -0700

    added functionality for everything but tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const producteData = await Product.findAll({
+      include: [{ model: Category}],
+    });
+    res.status(200).json(producteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const producteData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}],
+    });
+    res.status(200).json(producteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -92,7 +104,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 
